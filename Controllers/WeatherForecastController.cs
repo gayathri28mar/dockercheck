5f0632b2465fd3eb3e565d5f19fafd15d81124c8
Gayathri Mahadevan <gayat_19@yahoo.com>
2022-03-21 16:44:43 +0530

    yaml updated

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace ContainerApp.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freeze", "Bracing", "Dhilly", "goal", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
